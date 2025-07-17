d64c0cc13507ae26b3b9328836b731d6277173df
R11RZ <alpaka0102@gmail.com>
2025-07-18 01:06:41 +0300

    fix: changing the api request parameter in useUsers

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -4,7 +4,7 @@ const API_ENDPOINT = "https://dummyjson.com/users";
 
 const ORDER_DIRECTION = ["desc" , "asc"]
 
-export function useUsers(page = 1, inpage = 10 ,  sortBy , sortOrder) {
+export function useUsers(skip = 0, inpage = 10 ,  sortBy , sortOrder) {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -12,12 +12,13 @@ export function useUsers(page = 1, inpage = 10 ,  sortBy , sortOrder) {
   useEffect(() => {
     console.log("yes")
     setLoading(true)
-    fetch(API_ENDPOINT + `?limit=${inpage}&skip=${(page-1)*inpage}` + (sortBy && sortOrder ? `&sortBy=${sortBy}&order=${ORDER_DIRECTION[sortOrder-1]}`: "") )
+    fetch(API_ENDPOINT + `?limit=${inpage}&skip=${skip}` + (sortBy && sortOrder ? `&sortBy=${sortBy}&order=${ORDER_DIRECTION[sortOrder-1]}`: "") )
       .then((res) => res.json())
       .then(setUsers)
       .catch(setError)
       .finally(() => setLoading(false));
-  }, [page, inpage , sortBy , sortOrder]);
+  }, [skip, inpage , sortBy , sortOrder]);
+
   return [users , loading , error ]
 }
 
