c2657a0744ed2475a788c131c80b635d3defd988
R11RZ <alpaka0102@gmail.com>
2025-07-18 11:10:14 +0300

    feat: added hook useUser

---
+++ src/hooks/useUser.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const API_ENDPOINT = "https://dummyjson.com/users/";
+
+
+export function useUser(index) {
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(API_ENDPOINT + toString(index) )
+      .then((res) => res.json())
+      .then(setUser)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  }, [index]);
+
+  return [user , loading , error ]
+}
+
